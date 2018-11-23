869538cdc697ee2f1e7ddd621db41f8d07d3cfad
Knut Zuidema <knut.zuidema@gmail.com>
2018-11-23 20:01:57 +0100

    interface methods throw generic Exception for better usability

--- src/main/java/userManagement/UserManager.java
+++ src/main/java/userManagement/UserManager.java
@@ -11,27 +11,14 @@ public interface UserManager {
      * Add a user to the local store of users
      *
      * @param user User instance to be added
-     * @throws UserAlreadyExistsException when User with same id already exists in local store
-     * @throws IOException                if the file does not exist,
-     *                                    is a directory rather than a regular file,
-     *                                    or for some other reason cannot be opened for
-     *                                    reading.
-     * @throws ClassNotFoundException     if class of a serialized object cannot be
-     *                                    found.
      */
-    void addUser(User user) throws UserAlreadyExistsException, IOException, ClassNotFoundException;
+    void addUser(User user) throws Exception;
 
     /**
      * Verify that a User exists in local store
      *
      * @param user User to be verified
      * @return true if user exists, false if not
-     * @throws IOException            if the file does not exist,
-     *                                is a directory rather than a regular file,
-     *                                or for some other reason cannot be opened for
-     *                                reading.
-     * @throws ClassNotFoundException if class of a serialized object cannot be
-     *                                found.
      */
-    boolean verifyUser(User user) throws IOException, ClassNotFoundException;
+    boolean verifyUser(User user) throws Exception;
 }
